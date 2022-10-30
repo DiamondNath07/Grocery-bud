c9c6e34056152484e6c01943863eca99cfafff79
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-10-30 07:20:39 +0100

    select items for dynamical data

---
+++ app.js
@@ -0,0 +1,4 @@
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const groceryItem = document.querySelector('.grocery-item');
+const submitBtn = document.querySelector('.submit-btn');
